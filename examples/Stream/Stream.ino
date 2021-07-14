a2dc71559365a6e3431039678babe8453653065a
Mehmet Günce Akkoyun <akkoyun@me.com>
2021-07-14 15:58:29 +0300

    Update Stream.ino

--- examples/Stream/Stream.ino
+++ examples/Stream/Stream.ino
@@ -21,13 +21,15 @@ void loop() {
 	DataSet1.Stream_Statistic(132.2);
 
 	float Data_Array[] = {2.3, 3.5, 8.9, 11.25, 1.2};
-	DataSet2.Array_Average(Data_Array, 2)
+	float _Average = DataSet2.Array_Average(Data_Array, 2);
 
 	// Datas
 	Serial.print(DataSet1.Data_Count); Serial.println(" Point Stream Statistic Calculation...");
     Serial.print("Min Value: "); Serial.println(DataSet1.Stream_Minimum);
     Serial.print("Max Value: "); Serial.println(DataSet1.Stream_Maximum);
     Serial.print("Avg Value: "); Serial.println(DataSet1.Stream_Average);
+
+    Serial.print("Array Avg Value: "); Serial.println(_Average);
 
 	// Reset
 	DataSet1.Stream_Clear();
