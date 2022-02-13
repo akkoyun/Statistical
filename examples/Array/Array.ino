d0ec6c47d2aca2b38497ee6bad196c13ef2dc159
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-02-13 23:51:34 +0300

    Update

--- examples/Array/Array.ino
+++ examples/Array/Array.ino
@@ -13,6 +13,7 @@ void loop() {
 	// Set Data
 	Serial.println("Learning Data...");
 	float Data_Array[] = {220.14, 221.36, 218.21, 217.6, 215.7, 225.8, 220.14 };
+	int Data_Size = 7;
 
 	// Print Array
 	Serial.print("Data Array : ");
@@ -23,22 +24,19 @@ void loop() {
 	}
 	Serial.println("");
 
-	// Calculate Array Statistics
-	Stats.Array_Statistic(Data_Array, sizeof(Data_Array));
-
 	// Print Stats
-	Serial.print("Array Sum                      : "); Serial.println(Stats.Array_Sum);
-	Serial.print("Array Square Sum               : "); Serial.println(Stats.Array_Sq_Sum);
-	Serial.print("Array Min                      : "); Serial.println(Stats.Array_Min);
-	Serial.print("Array Max                      : "); Serial.println(Stats.Array_Max);
-	Serial.print("Array Aritmetic Average        : "); Serial.println(Stats.Array_AAvg);
-	Serial.print("Array Geometric Average        : "); Serial.println(Stats.Array_GAvg);
-	Serial.print("Array RMS Average              : "); Serial.println(Stats.Array_RMS_Avg);
-	Serial.print("Array Extended RMS Average     : "); Serial.println(Stats.Array_Ext_RMS_Avg);
-	Serial.print("Array Median                   : "); Serial.println(Stats.Array_Med);
-	Serial.print("Array Standart Deviation       : "); Serial.println(Stats.Array_SDev);
-	Serial.print("Array Standart Deviation Error : "); Serial.println(Stats.Array_SDev_Err);
-	Serial.print("Array Coefficient Factor       : "); Serial.println(Stats.Array_Coef);
+	Serial.print("Array Sum                      : "); Serial.println(Stats.Array_Sum(Data_Array, Data_Size));
+	Serial.print("Array Square Sum               : "); Serial.println(Stats.Array_Sq_Sum(Data_Array, Data_Size));
+	Serial.print("Array Min                      : "); Serial.println(Stats.Array_Min(Data_Array, Data_Size));
+	Serial.print("Array Max                      : "); Serial.println(Stats.Array_Max(Data_Array, Data_Size));
+	Serial.print("Array Aritmetic Average        : "); Serial.println(Stats.Array_Aritmetic_Average(Data_Array, Data_Size));
+	Serial.print("Array Geometric Average        : "); Serial.println(Stats.Array_Geometric_Average(Data_Array, Data_Size));
+	Serial.print("Array RMS Average              : "); Serial.println(Stats.Array_RMS_Average(Data_Array, Data_Size));
+	Serial.print("Array Extended RMS Average     : "); Serial.println(Stats.Array_Ext_RMS_Average(Data_Array, Data_Size));
+	Serial.print("Array Median                   : "); Serial.println(Stats.Array_Median(Data_Array, Data_Size));
+	Serial.print("Array Standart Deviation       : "); Serial.println(Stats.Array_Standart_Deviation(Data_Array, Data_Size));
+	Serial.print("Array Standart Deviation Error : "); Serial.println(Stats.Array_Standart_Deviation_Error(Data_Array, Data_Size));
+	Serial.print("Array Coefficient Factor       : "); Serial.println(Stats.Array_Coefficient_Factor(Data_Array, Data_Size));
 
 	Serial.print("Sorted Data Array              : ");
 	for (uint8_t i = 0; i < sizeof(Data_Array); i++) {
