20fc93816a95d9c80c1be6160cf23bb6175ffa6a
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-02-21 08:37:14 +0300

    01.03.02 - Update

--- install.sh
+++ install.sh
@@ -2,14 +2,13 @@
 
 # we need bash 4 for associative arrays
 if [ "${BASH_VERSION%%[^0-9]*}" -lt "4" ]; then
-  echo "BASH VERSION < 4: ${BASH_VERSION}" >&2
-  exit 1
+	echo "BASH VERSION < 4: ${BASH_VERSION}" >&2
+	exit 1
 fi
 
 # associative array for the platforms that will be verified in build_main_platforms()
-# this will be eval'd in the functions below because arrays can't be exported
-# Uno is ATmega328, Zero is SAMD21G18, ESP8266, Leonardo is ATmega32u4, M4 is SAMD51, Mega is ATmega2560, ESP32
-export MAIN_PLATFORMS='declare -A main_platforms=( [mega2560]="arduino:avr:mega:cpu=atmega2560" )'
+# Uno is ATmega328, Mega is ATmega2560
+export MAIN_PLATFORMS='declare -A main_platforms=( [uno]="arduino:avr:uno" [mega2560]="arduino:avr:mega:cpu=atmega2560" )'
 
 # make display available for arduino CLI
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_1.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :1 -ac -screen 0 1280x1024x16
@@ -18,137 +17,209 @@ export DISPLAY=:1.0
 
 #This condition is to avoid reruning install when build argument is passed
 if [[ $# -eq 0 ]] ; then
-# define colors
-GRAY='\033[1;30m';
-RED='\033[0;31m';
-LRED='\033[1;31m';
-GREEN='\033[0;32m';
-LGREEN='\033[1;32m';
-ORANGE='\033[0;33m';
-YELLOW='\033[1;33m';
-BLUE='\033[0;34m';
-LBLUE='\033[1;34m';
-PURPLE='\033[0;35m';
-LPURPLE='\033[1;35m';
-CYAN='\033[0;36m';
-LCYAN='\033[1;36m';
-LGRAY='\033[0;37m';
-WHITE='\033[1;37m';
 
+	# define colors
+	GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
 
-echo -e "\n########################################################################";
-echo -e "${YELLOW}INSTALLING ARDUINO IDE"
-echo "########################################################################";
+	echo -e "\n########################################################################";
+	echo -e "${YELLOW}INSTALLING ARDUINO IDE"
+	echo "########################################################################";
 
-# if .travis.yml does not set version
-if [ -z $ARDUINO_IDE_VERSION ]; then
-export ARDUINO_IDE_VERSION="1.8.11"
-echo "NOTE: YOUR .TRAVIS.YML DOES NOT SPECIFY ARDUINO IDE VERSION, USING $ARDUINO_IDE_VERSION"
-fi
+	# if .travis.yml does not set version
+	if [ -z $ARDUINO_IDE_VERSION ]; then
+		export ARDUINO_IDE_VERSION="1.8.11"
+		echo "NOTE: YOUR .TRAVIS.YML DOES NOT SPECIFY ARDUINO IDE VERSION, USING $ARDUINO_IDE_VERSION"
+	fi
 
-# if newer version is requested
-if [ ! -f $HOME/arduino_ide/$ARDUINO_IDE_VERSION ] && [ -f $HOME/arduino_ide/arduino ]; then
-echo -n "DIFFERENT VERSION OF ARDUINO IDE REQUESTED: "
-shopt -s extglob
-cd $HOME/arduino_ide/
-rm -rf *
-if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
-cd $OLDPWD
-fi
+	# if newer version is requested
+	if [ ! -f $HOME/arduino_ide/$ARDUINO_IDE_VERSION ] && [ -f $HOME/arduino_ide/arduino ]; then
+		echo -n "DIFFERENT VERSION OF ARDUINO IDE REQUESTED: "
+		shopt -s extglob
+		cd $HOME/arduino_ide/
+		rm -rf *
+		if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
+		cd $OLDPWD
+	fi
 
-# if not already cached, download and install arduino IDE
-echo -n "ARDUINO IDE STATUS: "
-if [ ! -f $HOME/arduino_ide/arduino ]; then
-echo -n "DOWNLOADING: "
-wget --quiet https://downloads.arduino.cc/arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz
-if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
-echo -n "UNPACKING ARDUINO IDE: "
-[ ! -d $HOME/arduino_ide/ ] && mkdir $HOME/arduino_ide
-tar xf arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz -C $HOME/arduino_ide/ --strip-components=1
-if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
-touch $HOME/arduino_ide/$ARDUINO_IDE_VERSION
-else
-echo -n "CACHED: "
-echo -e """$GREEN""\xe2\x9c\x93"
-fi
+	# if not already cached, download and install arduino IDE
+	echo -n "ARDUINO IDE STATUS: "
+	if [ ! -f $HOME/arduino_ide/arduino ]; then
+		echo -n "DOWNLOADING: "
+		wget --quiet https://downloads.arduino.cc/arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz
+		if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
+		echo -n "UNPACKING ARDUINO IDE: "
+		[ ! -d $HOME/arduino_ide/ ] && mkdir $HOME/arduino_ide
+		tar xf arduino-$ARDUINO_IDE_VERSION-linux64.tar.xz -C $HOME/arduino_ide/ --strip-components=1
+		if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
+		touch $HOME/arduino_ide/$ARDUINO_IDE_VERSION
 
-# define output directory for .hex files
-export ARDUINO_HEX_DIR=arduino_build_$TRAVIS_BUILD_NUMBER
+	else
 
-# link test library folder to the arduino libraries folder
-ln -s $TRAVIS_BUILD_DIR $HOME/arduino_ide/libraries/B100AA
+		echo -n "CACHED: "
+		echo -e """$GREEN""\xe2\x9c\x93"
+	fi
 
-# add the arduino CLI to our PATH
-export PATH="$HOME/arduino_ide:$PATH"
+	# define output directory for .hex files
+	export ARDUINO_HEX_DIR=arduino_build_$TRAVIS_BUILD_NUMBER
 
-echo -e "\n########################################################################";
-echo -e "${YELLOW}INSTALLING DEPENDENCIES"
-echo "########################################################################";
+	# link test library folder to the arduino libraries folder
+	ln -s $TRAVIS_BUILD_DIR $HOME/arduino_ide/libraries/Adafruit_Test_Library
 
-# install dependancy libraries in library.properties
-grep "depends=" $HOME/arduino_ide/libraries/B100AA/library.properties | sed 's/depends=//' | sed -n 1'p' |  tr ',' '\n' | while read word; do arduino --install-library "$word"; done
+	# add the arduino CLI to our PATH
+	export PATH="$HOME/arduino_ide:$PATH"
 
-# install the zero, esp8266, and adafruit board packages
-echo -n "ADD PACKAGE INDEX: "
-DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json" --save-prefs 2>&1)
-if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
+	echo -e "\n########################################################################";
+	echo -e "${YELLOW}INSTALLING DEPENDENCIES"
+	echo "########################################################################";
 
+	# install dependancy libraries in library.properties
+	grep "depends=" $HOME/arduino_ide/libraries/Adafruit_Test_Library/library.properties | sed 's/depends=//' | sed -n 1'p' |  tr ',' '\n' | while read word; do arduino --install-library "$word"; done
 
-INSTALL_AVR=$([[ $INSTALL_PLATFORMS == *"avr"* || -z "$INSTALL_PLATFORMS" ]] && echo 1 || echo 0)
+	# install the zero, esp8266, and adafruit board packages
+	echo -n "ADD PACKAGE INDEX: "
+	DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json" --save-prefs 2>&1)
+	if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
 
+	# This is a hack, we have to install by hand so lets delete it
+	echo "Removing ESP32 cache"
+	rm -rf ~/.arduino15/packages/esp32
+	echo -n "Current packages list:"
+	[ -d ~/.arduino15/packages/ ] && ls ~/.arduino15/packages/
 
-if [[ $INSTALL_AVR == 1 ]]; then
-  echo -n "ADAFRUIT AVR: "
-  DEPENDENCY_OUTPUT=$(arduino --install-boards adafruit:avr 2>&1)
-  if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96 OR CACHED"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
-fi
+	if [[ $INSTALL_ESP32 == 1 ]]; then
 
+		echo -n "ESP32: "
+		pip install pyserial
+		DEPENDENCY_OUTPUT=$(arduino --install-boards esp32:esp32 2>&1)
 
+		if [ $? -ne 0 ]; then 
+			echo -e "\xe2\x9c\x96 OR CACHED"; 
+		else 
+			echo -e """$GREEN""\xe2\x9c\x93"; 
+		fi
 
+	fi
 
+	if [[ $INSTALL_ZERO == 1 ]]; then
 
-# install random lib so the arduino IDE grabs a new library index
-# see: https://github.com/arduino/Arduino/issues/3535
-echo -n "UPDATE LIBRARY INDEX: "
-DEPENDENCY_OUTPUT=$(arduino --install-library USBHost > /dev/null 2>&1)
-if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
+		echo -n "ZERO: "
+		DEPENDENCY_OUTPUT=$(arduino --install-boards arduino:samd 2>&1)
 
+		if [ $? -ne 0 ]; then 
+			echo -e "\xe2\x9c\x96 OR CACHED"; 
+		else 
+			echo -e """$GREEN""\xe2\x9c\x93"; 
+		fi
 
+	fi
 
+	if [[ $INSTALL_ESP8266 == 1 ]]; then
+		
+		echo -n "ESP8266: "
+		DEPENDENCY_OUTPUT=$(arduino --install-boards esp8266:esp8266 2>&1)
 
-# set the maximal compiler warning level
-echo -n "SET BUILD PREFERENCES: "
-DEPENDENCY_OUTPUT=$(arduino --pref "compiler.warning_level=all" --save-prefs 2>&1)
-if [ $? -ne 0 ]; then echo -e """$RED""\xe2\x9c\x96"; else echo -e """$GREEN""\xe2\x9c\x93"; fi
+		if [ $? -ne 0 ]; then 
+			echo -e "\xe2\x9c\x96 OR CACHED"; 
+		else 
+			echo -e """$GREEN""\xe2\x9c\x93"; 
+		fi
 
-# init the json temp var for the current platform
-export PLATFORM_JSON=""
+	fi
 
-# init test stats counters
-export PASS_COUNT=0
-export SKIP_COUNT=0
-export FAIL_COUNT=0
-export PDE_COUNT=0
+	if [[ $INSTALL_AVR == 1 ]]; then
+
+		echo -n "ADAFRUIT AVR: "
+		DEPENDENCY_OUTPUT=$(arduino --install-boards adafruit:avr 2>&1)
+	
+		if [ $? -ne 0 ]; then 
+			echo -e "\xe2\x9c\x96 OR CACHED"; 
+		else 
+			echo -e """$GREEN""\xe2\x9c\x93"; 
+		fi
+	
+	fi
+
+	if [[ $INSTALL_SAMD == 1 ]]; then
+
+		echo -n "ADAFRUIT SAMD: "
+		DEPENDENCY_OUTPUT=$(arduino --install-boards adafruit:samd 2>&1)
+	
+		if [ $? -ne 0 ]; then 
+			echo -e "\xe2\x9c\x96 OR CACHED"; 
+		else 
+			echo -e """$GREEN""\xe2\x9c\x93"; 
+		fi
+
+	fi
+
+	if [[ $INSTALL_NRF52 == 1 ]]; then
+	
+		echo -n "ADAFRUIT NRF5X: "
+		pip install wheel
+		pip install setuptools
+		pip install adafruit-nrfutil
+		pip install pyserial
+		DEPENDENCY_OUTPUT=$(arduino --install-boards adafruit:nrf52 2>&1)
+	
+		if [ $? -ne 0 ]; then 
+			echo -e "\xe2\x9c\x96 OR CACHED"; 
+		else 
+			echo -e """$GREEN""\xe2\x9c\x93"; 
+		fi
+
+	fi
+
+	# install random lib so the arduino IDE grabs a new library index
+	# see: https://github.com/arduino/Arduino/issues/3535
+	echo -n "UPDATE LIBRARY INDEX: "
+	DEPENDENCY_OUTPUT=$(arduino --install-library "I2C_Functions")
+	if [ $? -ne 0 ]; then 
+		echo -e """$RED""\xe2\x9c\x96"; 
+	else 
+		echo -e """$GREEN""\xe2\x9c\x93"; 
+	fi
+
+	# set the maximal compiler warning level
+	echo -n "SET BUILD PREFERENCES: "
+	DEPENDENCY_OUTPUT=$(arduino --pref "compiler.warning_level=all" --save-prefs 2>&1)
+	if [ $? -ne 0 ]; then 
+		echo -e """$RED""\xe2\x9c\x96"; 
+	else 
+		echo -e """$GREEN""\xe2\x9c\x93"; 
+	fi
+
+	# init the json temp var for the current platform
+	export PLATFORM_JSON=""
+
+	# init test stats counters
+	export PASS_COUNT=0
+	export SKIP_COUNT=0
+	export FAIL_COUNT=0
+	export PDE_COUNT=0
+
 # close if [[ $# -eq 0 ]] ; then
 fi
-
-
-
 
 # build all of the examples for the passed platform
 #Sourcing and defining functions
 function build_platform() {
 
-	# arrays can't be exported, so we have to eval
-	eval $MAIN_PLATFORMS
+  # arrays can't be exported, so we have to eval
+  eval $MAIN_PLATFORMS
+  eval $AUX_PLATFORMS
+  eval $CPLAY_PLATFORMS
+  eval $M4_PLATFORMS
+  eval $ARCADA_PLATFORMS
+  eval $IO_PLATFORMS
+  eval $NRF5X_PLATFORMS
 
-	# reset platform json var
-	PLATFORM_JSON=""
+  # reset platform json var
+  PLATFORM_JSON=""
 
-	# expects argument 1 to be the platform key
-	local platform_key=$1
+  # expects argument 1 to be the platform key
+  local platform_key=$1
 
-	# placeholder for platform
+  # placeholder for platform
   local platform=""
 
   # track the exit code for this platform
@@ -178,16 +249,22 @@ function build_platform() {
   # grab the platform info from array or bail if invalid
   if [[ ${main_platforms[$platform_key]} ]]; then
     platform=${main_platforms[$platform_key]}
+  elif [[ ${aux_platforms[$platform_key]} ]]; then
+    platform=${aux_platforms[$platform_key]}
+  elif [[ ${cplay_platforms[$platform_key]} ]]; then
+    platform=${cplay_platforms[$platform_key]}
+  elif [[ ${m4_platforms[$platform_key]} ]]; then
+    platform=${m4_platforms[$platform_key]}
+  elif [[ ${arcada_platforms[$platform_key]} ]]; then
+    platform=${arcada_platforms[$platform_key]}
+  elif [[ ${io_platforms[$platform_key]} ]]; then
+    platform=${io_platforms[$platform_key]}
+  elif [[ ${nrf5x_platforms[$platform_key]} ]]; then
+    platform=${nrf5x_platforms[$platform_key]}
   else
     echo "NON-STANDARD PLATFORM KEY: $platform_key"
     platform=$platform_key
   fi
-
-
-
-
-
-
 
   echo -e "\n########################################################################";
 
@@ -354,8 +431,7 @@ function build_platform() {
 }
 
 # build all examples for every platform in $MAIN_PLATFORMS
-function build_main_platforms()
-{
+function build_main_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $MAIN_PLATFORMS
@@ -391,14 +467,19 @@ function build_main_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
 
 # build all examples for every platform in $AUX_PLATFORMS
-function build_aux_platforms()
-{
+function build_aux_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $AUX_PLATFORMS
@@ -434,12 +515,17 @@ function build_aux_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
-function build_cplay_platforms()
-{
+function build_cplay_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $CPLAY_PLATFORMS
@@ -475,13 +561,17 @@ function build_cplay_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
-
-function build_samd_platforms()
-{
+function build_samd_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $SAMD_PLATFORMS
@@ -517,13 +607,17 @@ function build_samd_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
-
-function build_m4_platforms()
-{
+function build_m4_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $M4_PLATFORMS
@@ -559,13 +653,17 @@ function build_m4_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
-
-function build_io_platforms()
-{
+function build_io_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $IO_PLATFORMS
@@ -601,15 +699,17 @@ function build_io_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
-
-
-
-function build_arcada_platforms()
-{
+function build_arcada_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $ARCADA_PLATFORMS
@@ -645,14 +745,17 @@ function build_arcada_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
 }
-
-
-function build_nrf5x_platforms()
-{
+function build_nrf5x_platforms() {
 
   # arrays can't be exported, so we have to eval
   eval $NRF5X_PLATFORMS
@@ -688,6 +791,12 @@ function build_nrf5x_platforms()
 
   done
 
+  # exit code is opposite of json build status
+  if [ $exit_code -eq 0 ]; then
+    json_main_platforms 1 "$platforms_json"
+  else
+    json_main_platforms 0 "$platforms_json"
+  fi
 
   return $exit_code
 
@@ -695,8 +804,7 @@ function build_nrf5x_platforms()
 
 
 # generate json string for a sketch
-function json_sketch()
-{
+function json_sketch() {
 
   # -1: skipped, 0: failed, 1: passed
   local status_number=$1
@@ -718,8 +826,7 @@ function json_sketch()
 }
 
 # generate json string for a platform
-function json_platform()
-{
+function json_platform() {
 
   # the platform key from main platforms or aux platforms
   local platform_key=$1
@@ -742,16 +849,42 @@ function json_platform()
 
 }
 
+# generate final json string
+function json_main_platforms() {
+
+  # 0: failed, 1: passed
+  local status_number=$1
+
+  # the json string for the main platforms
+  local platforms_json=$2
+
+  local repo=$(git config --get remote.origin.url)
+
+  echo -e "\n||||||||||||||||||||||||||||| JSON STATUS ||||||||||||||||||||||||||||||"
+
+  echo -n "{ \"repo\": \"$repo\", "
+  echo -n "\"status\": $status_number, "
+  echo -n "\"passed\": $PASS_COUNT, "
+  echo -n "\"skipped\": $SKIP_COUNT, "
+  echo -n "\"failed\": $FAIL_COUNT, "
+  echo "\"platforms\": { $platforms_json } }"
+
+  echo -e "||||||||||||||||||||||||||||| JSON STATUS ||||||||||||||||||||||||||||||\n"
+
+}
+
 #If there is an argument
 if [[ ! $# -eq 0 ]] ; then
-# define output directory for .hex files
-export ARDUINO_HEX_DIR=arduino_build_$TRAVIS_BUILD_NUMBER
 
-# link test library folder to the arduino libraries folder
-ln -s $TRAVIS_BUILD_DIR $HOME/arduino_ide/libraries/B100AA
+	# define output directory for .hex files
+	export ARDUINO_HEX_DIR=arduino_build_$TRAVIS_BUILD_NUMBER
 
-# add the arduino CLI to our PATH
-export PATH="$HOME/arduino_ide:$PATH"
+	# link test library folder to the arduino libraries folder
+	ln -s $TRAVIS_BUILD_DIR $HOME/arduino_ide/libraries/Adafruit_Test_Library
 
-"$@"
+	# add the arduino CLI to our PATH
+	export PATH="$HOME/arduino_ide:$PATH"
+
+	"$@"
+
 fi
