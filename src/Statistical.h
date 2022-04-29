86134597b4059498cab4398d4ad5b486ab289b5c
Mehmet Günce Akkoyun <akkoyun@me.com>
2022-04-29 08:45:36 +0300

    02.00.05

--- src/Statistical.h
+++ src/Statistical.h
@@ -1,11 +1,3 @@
-/* *******************************************************************************
- *  Copyright (C) 2014-2022 Mehmet Gunce Akkoyun Can not be copied and/or
- *	distributed without the express permission of Mehmet Gunce Akkoyun.
- *
- *	Library				: Data Statistical Library
- *	Code Developer		: Mehmet Gunce Akkoyun (akkoyun@me.com)
- *********************************************************************************/
-
 #ifndef __Statistical__
 #define __Statistical__
 
